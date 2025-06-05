d80fd705f9cfc8b4f36a8c64010fb8d60dfced69
KillerInk <30864229+KillerInk@users.noreply.github.com>
2025-06-05 16:19:27 +0200

    Fix chart tooltip reset on every data change (#951)
    * fix that chart tooltip selection get reseted on every data change
    
    * found more then suggested
    
    * bring back old data
    
    * optimize chart updating
    
    * remove not used lists
    
    * request and process history then request livedata.
    
    * remove leftover concat
    
    * move startGetLiveData to bottom as requested...

--- main/http_server/axe-os/src/app/components/home/home.component.ts
+++ main/http_server/axe-os/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { interval, map, Observable, shareReplay, startWith, switchMap, tap } from 'rxjs';
 import { HashSuffixPipe } from 'src/app/pipes/hash-suffix.pipe';
 import { QuicklinkService } from 'src/app/services/quicklink.service';
@@ -7,6 +7,7 @@ import { SystemService } from 'src/app/services/system.service';
 import { ThemeService } from 'src/app/services/theme.service';
 import { ISystemInfo } from 'src/models/ISystemInfo';
 import { ISystemStatistics } from 'src/models/ISystemStatistics';
+import { UIChart } from 'primeng/chart';
 
 
 @Component({
@@ -24,10 +25,6 @@ export class HomeComponent {
   public hashrateData: number[] = [];
   public temperatureData: number[] = [];
   public powerData: number[] = [];
-  public previousDataLabel: number[] = [];
-  public previousHashrateData: number[] = [];
-  public previousTemperatureData: number[] = [];
-  public previousPowerData: number[] = [];
   public chartData?: any;
 
   public maxPower: number = 0;
@@ -41,6 +38,8 @@ export class HomeComponent {
   public activePoolPort!: number;
   public activePoolUser!: string;
   public activePoolLabel!: 'Primary' | 'Fallback';
+  @ViewChild('chart')
+  private chart?: UIChart
 
   constructor(
     private systemService: SystemService,
@@ -86,6 +85,8 @@ export class HomeComponent {
     this.chartData = { ...this.chartData };
   }
 
+  
+
   private initializeChart() {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
@@ -99,7 +100,7 @@ export class HomeComponent {
         {
           type: 'line',
           label: 'Hashrate',
-          data: [],
+          data: [this.hashrateData],
           backgroundColor: primaryColor + '30',
           borderColor: primaryColor,
           tension: 0,
@@ -112,7 +113,7 @@ export class HomeComponent {
         {
           type: 'line',
           label: 'ASIC Temp',
-          data: [],
+          data: [this.temperatureData],
           fill: false,
           backgroundColor: textColorSecondary,
           borderColor: textColorSecondary,
@@ -193,8 +194,12 @@ export class HomeComponent {
       }
     };
 
+    this.chartData.labels = this.dataLabel;
+    this.chartData.datasets[0].data = this.hashrateData;
+    this.chartData.datasets[1].data = this.temperatureData;
+
     // load previous data
-    this.stats$ = this.systemService.getStatistics().pipe(shareReplay({refCount: true, bufferSize: 1}));
+    this.stats$ = this.systemService.getStatistics().pipe(shareReplay({ refCount: true, bufferSize: 1 }));
     this.stats$.subscribe(stats => {
       stats.statistics.forEach(element => {
         const idxHashrate = 0;
@@ -202,21 +207,25 @@ export class HomeComponent {
         const idxPower = 2;
         const idxTimestamp = 3;
 
-        this.previousHashrateData.push(element[idxHashrate] * 1000000000);
-        this.previousTemperatureData.push(element[idxTemperature]);
-        this.previousPowerData.push(element[idxPower]);
-        this.previousDataLabel.push(new Date().getTime() - stats.currentTimestamp + element[idxTimestamp]);
+        this.hashrateData.push(element[idxHashrate] * 1000000000);
+        this.temperatureData.push(element[idxTemperature]);
+        this.powerData.push(element[idxPower]);
+        this.dataLabel.push(new Date().getTime() - stats.currentTimestamp + element[idxTimestamp]);
 
-        if (this.previousHashrateData.length >= 720) {
-          this.previousHashrateData.shift();
-          this.previousTemperatureData.shift();
-          this.previousPowerData.shift();
-          this.previousDataLabel.shift();
+        if (this.hashrateData.length >= 720) {
+          this.hashrateData.shift();
+          this.temperatureData.shift();
+          this.powerData.shift();
+          this.dataLabel.shift();
         }
-      });
+      }),
+      this.startGetLiveData();
     });
+  }
 
-    // live data
+  private startGetLiveData()
+  {
+     // live data
     this.info$ = interval(5000).pipe(
       startWith(() => this.systemService.getInfo()),
       switchMap(() => {
@@ -230,29 +239,15 @@ export class HomeComponent {
           this.powerData.push(info.power);
           this.dataLabel.push(new Date().getTime());
 
-          if ((this.previousHashrateData.length + this.hashrateData.length) >= 720) {
-            if (this.previousHashrateData.length > 0) {
-              this.previousHashrateData.shift();
-              this.previousTemperatureData.shift();
-              this.previousPowerData.shift();
-              this.previousDataLabel.shift();
-            } else {
-              this.hashrateData.shift();
-              this.temperatureData.shift();
-              this.powerData.shift();
-              this.dataLabel.shift();
-            }
+          if ((this.hashrateData.length) >= 720) {
+            this.hashrateData.shift();
+            this.temperatureData.shift();
+            this.powerData.shift();
+            this.dataLabel.shift();
           }
-
-          this.chartData.labels = this.previousDataLabel.concat(this.dataLabel);
-          this.chartData.datasets[0].data = this.previousHashrateData.concat(this.hashrateData);
-          this.chartData.datasets[1].data = this.previousTemperatureData.concat(this.temperatureData);
-
-          this.chartData = {
-            ...this.chartData
-          };
         }
 
+        this.chart?.refresh();
         this.maxPower = Math.max(info.maxPower, info.power);
         this.nominalVoltage = info.nominalVoltage;
         this.maxTemp = Math.max(75, info.temp);
@@ -312,7 +307,7 @@ export class HomeComponent {
     const efficiencies = hashrateData.map((hashrate, index) => {
       const power = powerData[index] || 0;
       if (hashrate > 0) {
-        return power / (hashrate/1000000000000); // Convert to J/TH
+        return power / (hashrate / 1000000000000); // Convert to J/TH
       } else {
         return power; // in this case better than infinity or NaN
       }

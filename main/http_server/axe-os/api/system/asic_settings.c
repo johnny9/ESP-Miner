eef5851ebf683bd488d43c408dd946bca9c92d16
0xf0xx0 <33764485+0xf0xx0@users.noreply.github.com>
2025-06-11 17:07:02 -0400

    add board family to api (#966)

--- main/http_server/axe-os/api/system/asic_settings.c
+++ main/http_server/axe-os/api/system/asic_settings.c
@@ -36,7 +36,8 @@ esp_err_t GET_system_asic(httpd_req_t *req)
 
     // Add ASIC model to the JSON object
     cJSON_AddStringToObject(root, "ASICModel", GLOBAL_STATE->DEVICE_CONFIG.family.asic.name);
-    
+    cJSON_AddStringToObject(root, "boardFamily", GLOBAL_STATE->DEVICE_CONFIG.family.name);
+
     cJSON_AddNumberToObject(root, "defaultFrequency", GLOBAL_STATE->DEVICE_CONFIG.family.asic.default_frequency_mhz);
 
     // Create arrays for frequency and voltage options based on ASIC model

66e4f1e63172f69a212d116ed8963fe36da62cb7
WantClue <86001033+WantClue@users.noreply.github.com>
2025-06-18 14:30:54 +0200

    fix: process dirs recursively (#1046)

--- main/http_server/axe-os/only-gzip.js
+++ main/http_server/axe-os/only-gzip.js
@@ -3,28 +3,29 @@ const path = require('path');
 
 const directory = './dist/axe-os';
 
-fs.readdir(directory, (err, files) => {
-    if (err) throw err;
+function processDirectory(dirPath) {
+    fs.readdir(dirPath, (err, files) => {
+        if (err) throw err;
 
-    for (const file of files) {
-        const filePath = path.join(directory, file);
+        for (const file of files) {
+            const filePath = path.join(dirPath, file);
 
-        fs.stat(filePath, (err, stats) => {
-            if (err) throw err;
+            fs.stat(filePath, (err, stats) => {
+                if (err) throw err;
 
-            if (stats.isDirectory()) {
-                // If it's a directory, call rmdir after deleting its contents
-                fs.rm(filePath, { recursive: true }, (err) => {
-                    if (err) throw err;
-                    console.log(`Removed directory: ${filePath}`);
-                });
-            } else if (!file.endsWith('.gz')) {
-                // If it's a file and doesn't end with .gz, unlink it
-                fs.unlink(filePath, (err) => {
-                    if (err) throw err;
-                    console.log(`Removed file: ${filePath}`);
-                });
-            }
-        });
-    }
-});
+                if (stats.isDirectory()) {
+                    // If it's a directory, process it recursively
+                    processDirectory(filePath);
+                } else if (!file.endsWith('.gz')) {
+                    // If it's a file and doesn't end with .gz, unlink it
+                    fs.unlink(filePath, (err) => {
+                        if (err) throw err;
+                        console.log(`Removed file: ${filePath}`);
+                    });
+                }
+            });
+        }
+    });
+}
+
+processDirectory(directory);

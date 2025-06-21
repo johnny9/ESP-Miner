185b6e6009ed78b8dc767353ead76a5f93ec035d
duckAxe <210068897+duckAxe@users.noreply.github.com>
2025-06-21 15:41:17 +0200

    Refresh Logs Page (#1049)
    * Refresh Logs Page
    
    * Added filter icon
    
    * Clean up logs.component.ts
    
    * Clear logs on component destroy
    
    ---------
    
    Co-authored-by: duckAxe <>

--- main/http_server/axe-os/src/app/components/logs/logs.component.ts
+++ main/http_server/axe-os/src/app/components/logs/logs.component.ts
@@ -1,9 +1,7 @@
 import { AfterViewChecked, Component, Input, OnInit, ElementRef, OnDestroy, ViewChild, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { interval, map, Observable, shareReplay, startWith, Subscription, switchMap } from 'rxjs';
-import { SystemService } from 'src/app/services/system.service';
+import { Subscription } from 'rxjs';
 import { WebsocketService } from 'src/app/services/web-socket.service';
-import { ISystemInfo } from 'src/models/ISystemInfo';
 
 @Component({
   selector: 'app-logs',
@@ -14,54 +12,33 @@ export class LogsComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   public form!: FormGroup;
 
-  @ViewChild('scrollContainer') private scrollContainer!: ElementRef;
-  public info$: Observable<ISystemInfo>;
-
   public logs: { className: string, text: string }[] = [];
 
   private websocketSubscription?: Subscription;
-
-  public showLogs = false;
 
   public stopScroll: boolean = false;
 
   public isExpanded: boolean = false;
 
+  @ViewChild('scrollContainer') private scrollContainer!: ElementRef;
+
   @HostListener('document:keydown.esc', ['$event'])
-  onEscKey(event: KeyboardEvent) {
+  onEscKey() {
     if (this.isExpanded) {
       this.isExpanded = false;
     }
   }
+
   @Input() uri = '';
 
   constructor(
     private fb: FormBuilder,
     private websocketService: WebsocketService,
-    private systemService: SystemService
-  ) {
-
-
-    this.info$ = interval(5000).pipe(
-      startWith(() => this.systemService.getInfo()),
-      switchMap(() => {
-        return this.systemService.getInfo()
-      }),
-      map(info => {
-        info.power = parseFloat(info.power.toFixed(1))
-        info.voltage = parseFloat((info.voltage / 1000).toFixed(1));
-        info.current = parseFloat((info.current / 1000).toFixed(1));
-        info.coreVoltageActual = parseFloat((info.coreVoltageActual / 1000).toFixed(2));
-        info.coreVoltage = parseFloat((info.coreVoltage / 1000).toFixed(2));
-        return info;
-      }),
-      shareReplay({ refCount: true, bufferSize: 1 })
-    );
-
-
-  }
+  ) {}
 
   ngOnInit(): void {
+    this.subscribeLogs();
+
     this.form = this.fb.group({
       filter: ["", [Validators.required]]
     });
@@ -69,6 +46,7 @@ export class LogsComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   ngOnDestroy(): void {
     this.websocketSubscription?.unsubscribe();
+    this.clearLogs();
   }
 
   private subscribeLogs() {
@@ -102,16 +80,6 @@ export class LogsComponent implements OnInit, OnDestroy, AfterViewChecked {
           }
         }
       })
-  }
-
-  public toggleLogs() {
-    this.showLogs = !this.showLogs;
-
-    if (this.showLogs) {
-      this.subscribeLogs();
-    } else {
-      this.websocketSubscription?.unsubscribe();
-    }
   }
 
   public clearLogs() {
